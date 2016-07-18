e3a3a847be6dfcf86fbcfe03368977a84cc84988
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-18 14:06:30 +0100

    Body of constructor created as well as additional method to close the file when it is finished being read. Code added to ensure all current tests pass.

--- src/io/TrainReader.java
+++ src/io/TrainReader.java
@@ -1,19 +1,38 @@
 package io;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
- *
+ * Reads a training file that is selected by the user.
  * Created by George Shiangoli on 18/07/2016.
  */
 public class TrainReader implements CSVFileReader {
 
-    private File trainingFile;
+    private BufferedReader reader;
 
-    public TrainReader(String filePath) {}
+    public TrainReader(String filePath) {
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public int getLineCount() {
-        return 0;
+        return (int)reader.lines().count();
     }
+
+    @Override
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
