87845dc1283667dafe7d3174dcbece2fe95413fe
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-18 13:18:10 +0100

    Testing file created for Training data reader. First test written to test that an empty file results in a line count of 0.

---
+++ test/TrainReaderTest.java
@@ -0,0 +1,24 @@
+import io.CSVFileReader;
+import io.TrainReader;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+/**
+ * Tests for the TrainReader class of the io package.
+ * Created by George Shiangoli on 18/07/2016.
+ */
+public class TrainReaderTest {
+
+    private static final String PATH = "..test_resources" + File.separator;
+
+    private CSVFileReader reader;
+
+    @Test
+    public void testsGetLineCountOnEmptyFile() {
+        reader = new TrainReader(PATH + "empty.txt");
+        assertEquals(0, reader.getLineCount());
+    }
+
+}
