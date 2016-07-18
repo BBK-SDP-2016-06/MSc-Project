323b2ba229d7680bf0e8dc795da8987413d3fef1
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-18 13:17:14 +0100

    Additional functionality regarding retrieving the line count of the input file added.

---
+++ src/io/CSVFileReader.java
@@ -0,0 +1,18 @@
+package io;
+
+/**
+ * Charged with the handling of reading in the TEST and TRAINING files
+ * used for classification purposes. Valid files that can be read include
+ * .csv and .txt files. An attempt to open and read from a file that isn't
+ * of the aforementioned types result in an error.
+ * Created by George Shiangoli on 18/07/2016.
+ */
+public interface CSVFileReader {
+
+    /**
+     * Retrieves the number of lines within the file that is being read. This
+     * should equate to the number of time series data records present in the file.
+     * @return the number of lines in this file
+     */
+    int getLineCount();
+}
