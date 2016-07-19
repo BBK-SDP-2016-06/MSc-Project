f4e2b85369e066735d78f242ec779e8a73e42e45
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-19 10:43:01 +0100

    New exception created to handle missing time series data when a class label is present.

---
+++ src/exception/MissingTimeSeriesException.java
@@ -0,0 +1,8 @@
+package exception;
+
+/**
+ * This exception is thrown when a line includes a class label but
+ * no corresponding time series data.
+ * Created by George Shiangoli on 19/07/2016.
+ */
+public class MissingTimeSeriesException extends RuntimeException {}
