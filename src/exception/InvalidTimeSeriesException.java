b66026c1dd5996dabd964479a4ecc5ac57c1149e
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-19 10:16:22 +0100

    New exception created to handle errors within the time series data itself.

---
+++ src/exception/InvalidTimeSeriesException.java
@@ -0,0 +1,8 @@
+package exception;
+
+/**
+ * This exception is thrown when a time series reading is
+ * represented as a non numeric value.
+ * Created by George Shiangoli on 19/07/2016.
+ */
+public class InvalidTimeSeriesException extends RuntimeException {}
