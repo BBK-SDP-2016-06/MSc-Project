abcd635a1b6ad3eda160a59c1506b79cfdbe0796
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-20 15:48:20 +0100

    Testing class created for TimeSeriesImplementation. First testing method written for getClassType() functionality. Does not currently compile.

---
+++ test/TimeSeriesImplTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import structure.TimeSeries;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Testing class to assert behaviour of TimeSeries data structure.
+ * Created by George Shiangoli on 20/07/2016.
+ */
+public class TimeSeriesImplTest {
+
+    private List<Double> sampleData;
+
+    @Before
+    public void init() {
+        sampleData = Arrays.asList(1.5987, 2.9125, 3.0050, -1.2345, -5.1194);
+    }
+
+    @Test
+    public void testGetClassType() {
+        for (int i = 1; i <= 10; i++) {
+            TimeSeries series = new TimeSeriesImpl(i, sampleData);
+            assertEquals(i, series.getclassType());
+        }
+    }
+}
