9f50afe0a23205a152337856706210ae2ff2269c
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-20 18:26:15 +0100

    New test added to test new functionality of converting a string into a TimeSeries object. Test currently fails as expected.

--- test/InputUtilsTest.java
+++ test/InputUtilsTest.java
@@ -5,8 +5,11 @@ import exception.NoDataClassException;
 import io.InputUtils;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import structure.TimeSeries;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -57,4 +60,15 @@ public class InputUtilsTest {
 
     @Test (expected = MissingTimeSeriesException.class)
     public void missingTimeSeriesFails() { InputUtils.validateLine(incorrectData.get(4)); }
+
+    //Converting a string into TimeSeries object
+
+    @Test
+    public void testConvertingCorrectStringToTimeSeriesObject() {
+        TimeSeries series = InputUtils.toTimeSeries("1, 0.5, 1.5, 2.5, 3.5, 4.5");
+        assert series != null;
+        assertEquals(1, series.getClassType());
+        assertEquals(5, series.getDataSize());
+        assertTrue(Arrays.asList(0.5, 1.5, 2.5, 3.5, 4.5).equals(series.getData()));
+    }
 }
