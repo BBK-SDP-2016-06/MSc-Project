7a19f06a70d689485d18106e80017ff7418e63b8
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-21 12:00:33 +0100

    Test created to assert that entire data set is read correctly from example1 file and converted to correct time series format.

--- test/TestFileReaderImplTest.java
+++ test/TestFileReaderImplTest.java
@@ -2,9 +2,15 @@ import io.TestFileReader;
 import io.TestFileReaderImpl;
 import org.junit.Before;
 import org.junit.Test;
+import structure.TimeSeries;
+import structure.TimeSeriesImpl;
+
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Tests for the TestFileReaderImpl class of the io package.
@@ -14,12 +20,19 @@ public class TestFileReaderImplTest {
 
     private String path;
     private TestFileReader reader;
+    private List<TimeSeries> example1;
 
     @Before
     public void init() {
         path = new File("").getAbsolutePath() + File.separator
                 + "test_resources" + File.separator
                 + "TestReaderTest_Resources" + File.separator;
+        example1 = new ArrayList<>();
+        example1.add(new TimeSeriesImpl(1, Arrays.asList(0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09)));
+        example1.add(new TimeSeriesImpl(2, Arrays.asList(-0.01, -0.02, -0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09)));
+        example1.add(new TimeSeriesImpl(3, Arrays.asList(0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0)));
+        example1.add(new TimeSeriesImpl(2, Arrays.asList(-0.02, -0.04, -0.06, -0.08, -0.1, -0.12, -0.14, -0.16, -0.18)));
+        example1.add(new TimeSeriesImpl(5, Arrays.asList(3.01, 3.02, 3.03, 3.04, 3.05, 3.06, 3.07, 3.08, 3.09)));
     }
 
     @Test
@@ -44,6 +57,12 @@ public class TestFileReaderImplTest {
     public void testGetTimeSeriesCountOnExample1() {
         reader = new TestFileReaderImpl(path + "exampleTest1.txt");
         assertEquals(5, reader.getTimeSeriesCount());
+    }
+
+    @Test
+    public void testGetDataSetOnExample1() {
+        reader = new TestFileReaderImpl(path + "exampleTest1.txt");
+        assertEquals(example1, reader.getDataSet());
     }
 
 }
