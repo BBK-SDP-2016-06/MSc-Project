2abc06e0ecb4ebea1ad606599d60f8fde321c40d
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-22 15:49:21 +0100

    Interfaces created for pre-processing package.

---
+++ src/preprocessing/DataTransformer.java
@@ -0,0 +1,9 @@
+package preprocessing;
+
+/**
+ * Uses a DimensionReducer (optional) and a Discretizer to
+ * transform a TimeSeries object data structure to a symbolised
+ * String format (Word) that can be analysed by the Classifier.
+ * Created by George Shiangoli on 22/07/2016.
+ */
+public interface DataTransformer {}
---
+++ src/preprocessing/Discretizer.java
@@ -0,0 +1,8 @@
+package preprocessing;
+
+/**
+ * Transforms the reduced time series data representation into
+ * a symbolic String format known as a Word.
+ * Created by George Shiangoli on 22/07/2016.
+ */
+public interface Discretizer {}
