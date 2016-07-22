c71c78fc026e3ea8d6dab9465098f8eeae7d35fa
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-22 15:49:57 +0100

    Bare shell of main app entrance point created, extending java fx application.

---
+++ src/MainApp.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class represents the entry point for the application.
+ * Created by George Shiangoli on 21/07/2016.
+ */
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
