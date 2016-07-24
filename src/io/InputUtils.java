3a02e9448784c9118de2cb5d4f901acc426146e9
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-24 13:43:12 +0100

    Method for converting string into TimeSeries amended to assign -1 to unlabelled data. Confirmation that all tests currently pass.

--- src/io/InputUtils.java
+++ src/io/InputUtils.java
@@ -13,9 +13,25 @@ import java.util.stream.Stream;
  */
 public class InputUtils {
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static boolean isLabelled(List<String> dataElements) {
+        if(dataElements.isEmpty()) {
+            return false;
+        } else {
+            String firstElement = dataElements.get(0);
+            try {
+                Integer.parseInt(firstElement);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+    }
+
     public static TimeSeries toTimeSeries(String dataLine) {
         List<String> splitLine = Stream.of(dataLine.split(",")).collect(Collectors.toList());
-        int classLabel = Integer.parseInt(splitLine.remove(0));
+
+        int classLabel = isLabelled(splitLine) ? Integer.parseInt(splitLine.remove(0)) : -1;
         List<Double> timeSeriesData = splitLine.parallelStream()
                                            .map(Double::parseDouble)
                                            .collect(Collectors.toList());
