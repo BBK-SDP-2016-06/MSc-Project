795a6ed0af508fc6544f741c41c58e6c89a0197d
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-25 11:15:57 +0100

    Implementation for TrainingFileReader created.Confirmation that all tests still currently pass.

--- src/io/TrainingFileReaderImpl.java
+++ src/io/TrainingFileReaderImpl.java
@@ -1,5 +1,9 @@
 package io;
 
+import data.DataLengthRange;
+import data.TimeSeries;
+import exception.ExceptionHandler;
+
 /**
  * Reads a training file as specified by the application user, converting the data lines into
  * a list of TimeSeries objects.
@@ -9,5 +13,27 @@ public class TrainingFileReaderImpl extends FileReaderImpl implements TrainingFi
 
     public TrainingFileReaderImpl(String filePath) {
         super(filePath);
+        ExceptionHandler.validateTrainingTimeSeries(timeSeriesData);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public int getClassCount() {
+        return (int) timeSeriesData.parallelStream()
+                .map(TimeSeries::getClassType)
+                .distinct()
+                .count();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public DataLengthRange getTimeSeriesLength() {
+        long upperBound = timeSeriesData.parallelStream().mapToLong(TimeSeries::getDataSize).max().orElse(0);
+        long lowerBound = timeSeriesData.parallelStream().mapToLong(TimeSeries::getDataSize).min().orElse(0);
+        return new DataLengthRange(lowerBound, upperBound);
     }
 }
