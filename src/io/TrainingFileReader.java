7b4e266aedda9fb4fa94c9e3f5354beb02ac3531
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-25 11:15:14 +0100

    Additional functionality specific to training file added to interface.

--- src/io/TrainingFileReader.java
+++ src/io/TrainingFileReader.java
@@ -1,9 +1,25 @@
 package io;
 
+import data.DataLengthRange;
+
 /**
  * A file reader used convert a training file into a list of TimeSeries objects
  * that can be analysed and understood by the application.
  * Created by George Shiangoli on 23/07/2016.
  */
 public interface TrainingFileReader extends FileReader {
+
+    /**
+     * Returns the total number of class labels within the training file read by
+     * this object.
+     * @return the number of classes / categories the training data is split into.
+     */
+    int getClassCount();
+
+    /**
+     * Returns a DataLengthRange object representing the range of time series lengths
+     * present in the training file read by this object.
+     * @return the range of lengths of time series data.
+     */
+    DataLengthRange getTimeSeriesLength();
 }
