901d6f1dbebdd963811a57a9fd4c4b35b9586db8
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-26 12:02:08 +0100

    New class created to handle utility methods relating to mathematical functions to be performed on the time series data. Skeleton of first method getMean created.

---
+++ src/preprocessing/MathUtils.java
@@ -0,0 +1,15 @@
+package preprocessing;
+
+import java.util.List;
+
+/**
+ * A utility class to hold various mathematical functions to help with the
+ * pre-processing steps of the application model.
+ * Created by George Shiangoli on 25/07/2016.
+ */
+public class MathUtils {
+
+    public static double getMean(List<Double> inputData) {
+        return 0.0;
+    }
+}
