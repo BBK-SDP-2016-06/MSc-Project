ea542d6d443b004f528e5bafa66173c8568db9a6
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-26 12:22:37 +0100

    JavaDoc comment added getMean method.

--- src/preprocessing/MathUtils.java
+++ src/preprocessing/MathUtils.java
@@ -9,6 +9,12 @@ import java.util.List;
  */
 public class MathUtils {
 
+    /**
+     * Calculates the mean of the input data by summing the values and then
+     * dividing the intermediary result by the number of values.
+     * @param inputData the list of values
+     * @return the mean of the list of values. Returns NaN if the inputData is empty.
+     */
     public static double getMean(List<Double> inputData) {
         return (inputData.parallelStream().mapToDouble(d -> d).sum()) / inputData.size();
     }
