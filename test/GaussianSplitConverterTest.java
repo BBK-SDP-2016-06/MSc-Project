5260b5f3eea0dccc7209b39e865fd3ee45e0c6e2
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-27 17:03:35 +0100

    Methods added to test basic functionality of converting a reduced time series representation into a String (sequence of characters).

--- test/GaussianSplitConverterTest.java
+++ test/GaussianSplitConverterTest.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
+import preprocessing.DataConverter;
 import preprocessing.GaussianSplitConverter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Tests for the GaussianSplitConverter class of the preprocessing package.
@@ -25,6 +30,24 @@ public class GaussianSplitConverterTest {
     @Test (expected = IllegalArgumentException.class)
     public void testConstructorWithAlphabetSizeGreaterThan26Fails() {
         new GaussianSplitConverter(27);
+    }
+
+    @Test
+    public void testToWordWithSmallestAlphabetAndSingleValue() {
+        DataConverter converter = new GaussianSplitConverter(2);
+        assertEquals("a", converter.toWord(Collections.singletonList(-0.2345)));
+    }
+
+    @Test
+    public void testToWordWithSmallestAlphabet() {
+        DataConverter converter = new GaussianSplitConverter(2);
+        assertEquals("abbba", converter.toWord(Arrays.asList(-0.2345, 1.1, 0.556, 0.00001,-0.5)));
+    }
+
+    @Test
+    public void testToWordWithSmallestAlphabetAndBorderlineValue() {
+        DataConverter converter = new GaussianSplitConverter(2);
+        assertEquals("ab", converter.toWord(Arrays.asList(-0.01, 0.0)));
     }
 
 }
