31104913cd8c235afd64f9fc3e7d0745cfbea9e9
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-28 14:16:15 +0100

    New testing class created for new DiscretizedData structure. Confirmation that all tests currently pass.

---
+++ test/DiscretizedDataImplTest.java
@@ -0,0 +1,45 @@
+import data.DiscretizedData;
+import data.DiscretizedDataImpl;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Testing class to assert behaviour of DiscretizedData data structure.
+ * Created by George Shiangoli on 28/07/2016.
+ */
+public class DiscretizedDataImplTest {
+
+    @Test
+    public void testGetClassType() {
+        for (int i = 1; i <= 10; i++) {
+            DiscretizedData data = new DiscretizedDataImpl(i, "abcd");
+            assertEquals(i, data.getClassType());
+        }
+    }
+
+    @Test
+    public void testGetWord() {
+        DiscretizedData data = new DiscretizedDataImpl(1, "abcd");
+        assertEquals("abcd", data.getWord());
+    }
+
+    @Test
+    public void minusOneClassTypeDoesNotFail() {
+        new DiscretizedDataImpl(-1, "abcd");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void negativeClassTypeFails() {
+        new DiscretizedDataImpl(-2, "abcd");
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void nullWordFails() {
+        new DiscretizedDataImpl(-2, null);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void emptyWordFails() {
+        new DiscretizedDataImpl(-2, "");
+    }
+}
