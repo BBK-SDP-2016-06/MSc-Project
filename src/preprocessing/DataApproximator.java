c580dc6646cd2a121f71bd24906576940dae783f
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-28 09:56:14 +0100

    Additional information added to Javadoc comments.

--- src/preprocessing/DataApproximator.java
+++ src/preprocessing/DataApproximator.java
@@ -5,7 +5,8 @@ import java.util.List;
 /**
  * Transforms a time series data set into new approximation in order to
  * both reduce the dimensionality and complexity. This enables further
- * pre-processing and classification to be performed with greater efficiency.
+ * pre-processing and classification to be performed with greater efficiency
+ * on the selected time series data.
  * Created by George Shiangoli on 25/07/2016.
  */
 public interface DataApproximator {
