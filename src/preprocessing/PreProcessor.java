22eb232a0b90be89487a25baf2d472c0146728ba
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-28 11:43:54 +0100

    Class renamed to PreProcessor and TimeSeries field removed.

--- src/preprocessing/PreProcessor.java
+++ src/preprocessing/PreProcessor.java
@@ -11,27 +11,25 @@ import static preprocessing.MathUtils.*;
  * a symbolic String format known as a Word.
  * Created by George Shiangoli on 22/07/2016.
  */
-public abstract class Discretizer {
+public abstract class PreProcessor {
 
     protected DataApproximator approximator;
     protected DataConverter converter;
-    private TimeSeries rawData;
 
-    public Discretizer(TimeSeries rawData, DataApproximator approximator, DataConverter converter) {
-        this.rawData = rawData;
+    public PreProcessor(DataApproximator approximator, DataConverter converter) {
         this.approximator = approximator;
         this.converter = converter;
     }
 
     /**
-     * Executes the discretization process upon the TimeSeries data object used to construct
-     * this object. This involves checking first whether the data is in a z-normalized form
-     * and performing the normalization process if required. The normalized data is then
-     * transformed into a reduced format before being converting into a String word ready
-     * for the classification process.
+     * Executes the discretization process upon a TimeSeries data object. This involves checking
+     * first whether the data is in a z-normalized form and performing the normalization process if
+     * required. The normalized data is then transformed into a reduced format before being converting
+     * into a String word ready for the classification process.
+     * @param rawData the TimeSeries object upon which the discretization proces will take place.
      * @return a String representation of the time series data of this TimeSeries object.
      */
-    public final String executeDiscretization() {
+    public final String discretize(TimeSeries rawData) {
         List<Double> normalizedData = normalizeData(rawData.getData());
         List<Double> transformedData = approximator.reduce(normalizedData);
         return converter.toWord(transformedData);
