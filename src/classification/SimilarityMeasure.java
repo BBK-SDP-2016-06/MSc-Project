1a5c67f16ba7ccbbb794edaa8ed0cb3720df49de
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-07-28 15:03:16 +0100

    Javadoc comments added to DistanceMeasure interface.

--- src/classification/SimilarityMeasure.java
+++
@@ -1,9 +0,0 @@
-package classification;
-
-/**
- * A class that represents the group of algorithms charged
- * with assessing the similarity between two Strings obtained
- * from a DataApproximator.
- * Created by Shiangoli George on 22/07/2016.
- */
-public interface SimilarityMeasure {}
