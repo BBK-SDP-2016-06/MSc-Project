06dcbe73ce43195d4b5f9ba25c024b9dd8993e38
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-08-05 11:36:32 +0100

    Initial introduction layout and controller created.

---
+++ src/classificationApp/view/IntroductionController.java
@@ -0,0 +1,27 @@
+package classificationApp.view;
+
+import classificationApp.MainApp;
+import javafx.fxml.FXML;
+
+/**
+ * Controller class for the Introduction window.
+ * Created by George on 05/08/2016.
+ */
+public class IntroductionController {
+
+    private MainApp mainApp;
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    @FXML
+    private void handleStart() {
+        mainApp.showTrainingDataLoader();
+    }
+
+    @FXML
+    private void handleExit() {
+        System.exit(0);
+    }
+}
