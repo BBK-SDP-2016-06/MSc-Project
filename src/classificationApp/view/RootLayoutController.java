8d7456efa5f7c3706050f54fbf5c0cfde6186082
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-08-15 14:24:06 +0100

    RootLayout gui and controller classes completed.

---
+++ src/classificationApp/view/RootLayoutController.java
@@ -0,0 +1,58 @@
+package classificationApp.view;
+
+import classificationApp.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+/**
+ * Controller class for the root view (top menu bar of application).
+ * Created by George Shiangoli on 13/08/2016.
+ */
+public class RootLayoutController {
+
+    @FXML
+    private Menu fileMenu;
+
+    @FXML
+    private MenuItem changeMode;
+
+    @FXML
+    private MenuItem close;
+
+    @FXML
+    private Menu helpMenu;
+
+    @FXML
+    private MenuItem about;
+
+    private MainApp mainApp;
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    @FXML
+    private void handleChangeMode() {
+        if (mainApp.getMode().equals("Classification")) {
+            mainApp.showAnimationLayout();
+        } else {
+            mainApp.showClassificationLayout();
+        }
+    }
+
+    @FXML
+    private void handleClose() {
+        mainApp.closeApplication();
+    }
+
+    @FXML
+    private void handleAbout() {
+        Alert infoAlert = new Alert(Alert.AlertType.INFORMATION);
+        infoAlert.setTitle("About");
+        infoAlert.setHeaderText("");
+        infoAlert.setContentText("Author: George Shiangoli 2016");
+        infoAlert.showAndWait();
+    }
+}
