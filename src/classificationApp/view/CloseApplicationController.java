5b79ed117b6d4c22d4b8c412cbd3e105f338a554
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-08-15 14:04:17 +0100

    New controller class added for CloseApplication fxml layout.

---
+++ src/classificationApp/view/CloseApplicationController.java
@@ -0,0 +1,27 @@
+package classificationApp.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Controller class for the CloseApplication dialogue.
+ * Created by George Shiangoli on 15/08/2016.
+ */
+public class CloseApplicationController {
+
+    @FXML
+    private Label closeAppQuestion;
+
+    @FXML
+    private Button yesButton;
+
+    @FXML
+    private Button noButton;
+
+    @FXML
+    private void handleYesButton() {}
+
+    @FXML
+    private void handleNoButton() {}
+}
