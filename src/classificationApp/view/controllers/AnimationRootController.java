e98a7e62f5e3daafdf86fe9b38ada295e65822ae
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-08-19 07:14:49 +0100

    Controller class for Animation Root now contains references to progress images.

--- src/classificationApp/view/controllers/AnimationRootController.java
+++ src/classificationApp/view/controllers/AnimationRootController.java
@@ -2,191 +2,128 @@ package classificationApp.view.controllers;
 
 import classificationApp.MainApp;
 import classificationApp.model.data.TimeSeries;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 /**
  * Controller class for the animation mode of the application.
- * Created by George Shiangoli on 13/08/2016.
+ * Created by George Shiangoli on 17/08/2016.
  */
 public class AnimationRootController {
 
     @FXML
-    private GridPane gridPaneLayout;
+    private GridPane gridPane;
 
     @FXML
-    private Label rawDataRepresentation;
+    private Label title;
 
     @FXML
-    private Label normalization;
+    private TextArea description;
 
     @FXML
-    private Label pAA;
+    private Button startAgain;
 
     @FXML
-    private Label discretization;
+    private Button exit;
 
     @FXML
-    private Label similarityMeasure;
+    private ImageView testSelectionIcon;
 
     @FXML
-    private Label kNNClassification;
+    private ImageView arrow1;
 
     @FXML
-    private Label result;
+    private ImageView rawDataIcon;
 
     @FXML
-    private Label trainingFileName;
+    private ImageView arrow2;
 
     @FXML
-    private Label trainingFileClassCount;
+    private ImageView normalizedDataIcon;
 
     @FXML
-    private Label trainingFileDataSetSize;
+    private ImageView arrow3;
 
     @FXML
-    private Label trainingFileTimeSeriesLength;
+    private ImageView partitionIcon;
 
     @FXML
-    private TextArea stageDescription;
+    private ImageView arrow4;
 
     @FXML
-    private Button nextButton;
+    private ImageView meanIcon;
 
     @FXML
-    private Button prevButton;
+    private ImageView arrow5;
 
     @FXML
-    private Button exitButton;
+    private ImageView gaussianIcon;
+
+    @FXML
+    private ImageView arrow6;
+
+    @FXML
+    private ImageView lcsIcon;
+
+    @FXML
+    private ImageView knnIcon;
 
     private MainApp mainApp;
-    private IntegerProperty stageNumber;
-
-    private TimeSeries rawTestSample;
-    private TimeSeries normalizedTestSample;
 
     @FXML
     private void initialize() {
-        stageNumber = new SimpleIntegerProperty();
-    }
-
-    public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
-        stageNumber.addListener((observable, oldValue, newValue) -> {
-            removeStyleClasses();
-            prevButton.setDisable(newValue.intValue() == 1);
-            switch (newValue.intValue()) {
-                case 1: //Select test sample / training file
-                    rawDataRepresentation.getStyleClass().add("currentStageLabel");
-                    stageDescription.setText("Select test data sample and click next to begin " +
-                            "classification model walk-through.");
-                    loadWalkThroughStep("view/TestSelection.fxml");
-                    break;
-                case 2: //Raw data representation
-                    rawDataRepresentation.getStyleClass().add("currentStageLabel");
-                    break;
-                case 3: //Normalization process
-                    normalization.getStyleClass().add("currentStageLabel");
-                    break;
-                case 4: //Frame partitioning
-                    pAA.getStyleClass().add("currentStageLabel");
-                    break;
-                case 5: //Average computed within each partition
-                    pAA.getStyleClass().add("currentStageLabel");
-                    break;
-                case 6: //Gaussian Distribution
-                    pAA.getStyleClass().add("currentStageLabel");
-                    break;
-                case 7: //Discretization
-                    discretization.getStyleClass().add("currentStageLabel");
-                    break;
-                case 8: //Similarity measure - string selection
-                    similarityMeasure.getStyleClass().add("currentStageLabel");
-                    break;
-                case 9: //Similarity measure - matrix creation
-                    similarityMeasure.getStyleClass().add("currentStageLabel");
-                    break;
-                case 10: //Similarity measure - result
-                    similarityMeasure.getStyleClass().add("currentStageLabel");
-                    break;
-                case 11: //kNN classification
-                    kNNClassification.getStyleClass().add("currentStageLabel");
-                    break;
-                case 12: //result
-                    result.getStyleClass().add("currentStageLabel");
-                    break;
-                default:
-                    break;
-            }
-        });
-        stageNumber.set(1);
-        setTRAIN();
+        title.setText("1. Test Data Selection");
+        description.setText("Select a data sample from a chosen file and click start to begin a walk-through" +
+                " of the classification process used within this model.");
     }
 
     @FXML
-    private void handlePrevButton() {
-        stageNumber.set(stageNumber.getValue() - 1);
+    private void handleStartAgainButtonPress() {
+        mainApp.showAnimationLayout();
+        startAgain.setDisable(true);
     }
 
     @FXML
-    private void handleNextButton() {
-        stageNumber.set(stageNumber.getValue() + 1);
-    }
-
-    @FXML
-    private void handleExitButton() {
+    private void handleExitButtonPress() {
         mainApp.closeApplication();
     }
 
-    private void removeStyleClasses() {
-        ObservableList<Node> gridPaneNodes = gridPaneLayout.getChildren();
-        VBox stageList = null;
-        for(Node n : gridPaneNodes) {
-            if (n instanceof VBox) {
-                stageList = (VBox)n;
-            }
-        }
-        if (stageList != null) {
-            stageList.getChildren().forEach(n -> n.getStyleClass().clear());
-        }
-    }
-
-    private void loadWalkThroughStep(String filePath) {
+    public void showDataVisualisation(TimeSeries dataSample) {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource(filePath));
-            AnchorPane anchorPane = loader.load();
-            gridPaneLayout.add(anchorPane, 2, 0);
-            ClassificationStepController controller = loader.getController();
-            controller.setController(this);
+            loader.setLocation(MainApp.class.getResource("view/DataVisualisation.fxml"));
+            AnchorPane dataVisualisationLayout = loader.load();
+            gridPane.getChildren().remove(5);
+            gridPane.add(dataVisualisationLayout, 1, 1);
+            GridPane.setColumnSpan(dataVisualisationLayout, GridPane.REMAINING);
+            DataVisualisationController visController = loader.getController();
+            visController.setRootController(this);
+            visController.setTestSample(dataSample);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void setTRAIN() {
-        if (mainApp.getTrainingData() == null) {
-            trainingFileName.setText("Name:");
-            trainingFileClassCount.setText("0");
-            trainingFileDataSetSize.setText("0");
-            trainingFileTimeSeriesLength.setText("0");
-        } else {
-            trainingFileName.setText("Name: " + mainApp.getTrainingData().getFile().getName());
-            trainingFileClassCount.setText(String.valueOf(mainApp.getTrainingData().getClassCount()));
-            trainingFileDataSetSize.setText(String.valueOf(mainApp.getTrainingData().getTimeSeriesCount()));
-            trainingFileTimeSeriesLength.setText(mainApp.getTrainingData().getTimeSeriesLength().toString());
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/TestDataSelection.fxml"));
+            AnchorPane testSelectView = loader.load();
+            gridPane.add(testSelectView, 1, 1);
+            GridPane.setColumnSpan(testSelectView, GridPane.REMAINING);
+            TestDataSelectionController testController = loader.getController();
+            testController.setRootController(this);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -194,23 +131,4 @@ public class AnimationRootController {
         return mainApp;
     }
 
-    public void setNextButtonDisabled(boolean isDisabled) {
-        nextButton.setDisable(isDisabled);
-    }
-
-    public TimeSeries getRawTestSample() {
-        return rawTestSample;
-    }
-
-    public void setRawTestSample(TimeSeries rawTestSample) {
-        this.rawTestSample = rawTestSample;
-    }
-
-    public TimeSeries getNormalizedTestSample() {
-        return normalizedTestSample;
-    }
-
-    public void setNormalizedTestSample(TimeSeries normalizedTestSample) {
-        this.normalizedTestSample = normalizedTestSample;
-    }
 }
