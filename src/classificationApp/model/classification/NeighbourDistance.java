1f83ddbf04ad0d37b86bfad2997a37b85400a2f7
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-08-27 10:24:46 +0100

    Removed boilerplate code using project lombok. Confirmation that all tests still currently pass.

--- src/classificationApp/model/classification/NeighbourDistance.java
+++ src/classificationApp/model/classification/NeighbourDistance.java
@@ -1,37 +1,16 @@
 package classificationApp.model.classification;
 
+import lombok.*;
+
 /**
  * Holds information about a training classificationApp.model.data sample's class type
  * and its computed distance from the test sample.
  * Created by George Shiangoli on 30/07/2016.
  */
+@Data
+@AllArgsConstructor
 public class NeighbourDistance {
 
     private int classType, distance;
 
-    public NeighbourDistance(int classType, int distance) {
-        setClassType(classType);
-        setDistance(distance);
-    }
-
-    public int getClassType() {
-        return classType;
-    }
-
-    public void setClassType(int classType) {
-        this.classType = classType;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public void setDistance(int distance) {
-        this.distance = distance;
-    }
-
-    @Override
-    public String toString() {
-        return classType + "-" + distance;
-    }
 }
