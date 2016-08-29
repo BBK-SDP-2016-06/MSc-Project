1b13617d3edf1db5a09d9065c8e4f8aafcb5707c
George Shiangoli <gshian01@mail.bbk.ac.uk>
2016-08-29 12:18:51 +0100

    Amended bug resulting from lombok annotation.

--- src/classificationApp/model/classification/NeighbourDistance.java
+++ src/classificationApp/model/classification/NeighbourDistance.java
@@ -1,17 +1,16 @@
 package classificationApp.model.classification;
 
-import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Holds information about a training data sample's class type
  * and its computed distance from the test sample.
  * Created by George Shiangoli on 30/07/2016.
  */
-@Data
+
 @AllArgsConstructor
+@Getter
 public class NeighbourDistance {
-
     private int classType, distance;
-
 }
